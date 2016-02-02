8aebaafa47e611f7f583cffe3c38ce392fc11d77
jfanzago <jfanzago@gmail.com>
2016-02-02 17:50:53 -0500

    Initial commit on address book

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Contact.create(name:"Jeanella", email:"jfanzago@gmail.com", address:"2210 se 4th ave. fort lauderdale, fl 33316", phone_number: '7273264501')
+Contact.create(name:"Mike", email:"mike@gmail.com", address:"2210 se 4th ave. fort lauderdale, fl 33316", phone_number: '9546756677')
+Contact.create(name:"Joe", email:"jow@gmail.com", address:"7728 se 5th ave. fort lauderdale, fl 33316", phone_number: '7278390284')
+Contact.create(name:"Jane", email:"jane@gmail.com", address:"25 se 10th st. fort lauderdale, MI 33316", phone_number: '5618729393')
+Contact.create(name:"Barbie", email:"barbie@gmail.com", address:"879 se 87th ave. fort lauderdale, fl 33316", phone_number: '5619308903')
